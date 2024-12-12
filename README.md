2f570e06b5bb72fef07e020af06245c96e5d0aaf
mariaserov <serovmari@gmail.com>
2024-12-12 15:07:56 +0000

    Wrote README file and added comments

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# kaggle_childmind_hdaml
+# The Kaggle ChildMind project
+https://www.kaggle.com/competitions/child-mind-institute-problematic-internet-use
+
+### PCIAT -> sii function: 
+The target variable, sii, is derived directly from PCIAT_Total, which is a sum of the 20 PCIAT columns (questionnaire answers).
+
+The wrapper function 'pciat_sii_pred' was written, which allows to run an ML model to predict each of the 20 PCIAT values and map them to sii (sii), rather than predicting sii directly. This was shown to improve metrics (accuracy, f1 score) for a basic decision tree model. 
+
+To run function, make sure the 'func_pciat.py' file is in your directory, and import the function from there (the file is in the 'src' folder). Then initialise a model object (eg with scikit-learn), and supply the training data as well as the model to  'pciat_sii_pred' function :). NOTE THAT THE MODEL DOES THE TRAIN TEST SPLIT ITSELF (see details below on reason for this in details). For example:
+
+```python 
+from func_pciat import pciat_sii_pred
+train = pd.read_csv('../data/train.csv')
+model = DecisionTreeClassifier()
+
+y_pred, y_test = pciat_sii_pred(train, model)
+
+```
+
+Some important notes on this: 
+- The function does the train test split itself. This is because the train / test split needs to be done BEFORE x / y (ie features and targets) split, to ensure that PCIAT values are predicted for the same set of observations, so that they can be re-combined to yield our sii prediction.  
+- The function doesn't (yet?) work for cross-validation or where multiple models are trained as part of same initialised model object. 
+
+
+More info can be found in docstrings of the function.
--- src/func_pciat.py
+++ src/func_pciat.py
@@ -35,36 +35,55 @@ def pciat_sii_pred(data, model):
        y_test, y_pred -> a dataframe with predicted values, as well as a dataframe with test values which can then be assessed, e.g. with an f1 metric.
     """
     time1 = time.time()
+
+    # Define the PCIAT columns
     pciat_vals = ['PCIAT-PCIAT_01', 'PCIAT-PCIAT_02', 'PCIAT-PCIAT_03', 'PCIAT-PCIAT_04',
        'PCIAT-PCIAT_05', 'PCIAT-PCIAT_06', 'PCIAT-PCIAT_07', 'PCIAT-PCIAT_08',
        'PCIAT-PCIAT_09', 'PCIAT-PCIAT_10', 'PCIAT-PCIAT_11', 'PCIAT-PCIAT_12',
        'PCIAT-PCIAT_13', 'PCIAT-PCIAT_14', 'PCIAT-PCIAT_15', 'PCIAT-PCIAT_16',
        'PCIAT-PCIAT_17', 'PCIAT-PCIAT_18', 'PCIAT-PCIAT_19', 'PCIAT-PCIAT_20']
+    
+    # Train test split
+    
     train, test = train_test_split(data, test_size=0.3, random_state=42)
+
+    # Drops NAs of PCIAT form train & test
+
     train1 = train.dropna(subset=pciat_vals)
     test1 = test.dropna(subset=pciat_vals)
 
-    train2 = train1.drop("sii", axis=1).dropna(subset=pciat_vals).select_dtypes(include=['float64', 'int64'])
-    test2 = test1.drop("sii", axis=1).dropna(subset=pciat_vals).select_dtypes(include=['float64', 'int64'])
-    # data = data.drop("sii", axis=1).dropna(subset=pciat_vals)
-    # print(train.columns)
-    # print(test.columns)
+    # Drop sii columns, include only float and integer values 
+
+    train2 = train1.drop("sii", axis=1).select_dtypes(include=['float64', 'int64'])
+    test2 = test1.drop("sii", axis=1).select_dtypes(include=['float64', 'int64'])
+
+    # Initialise a df where we store the predicted values
+
     pciat_pred = pd.DataFrame(index=range(test1.shape[0]))
+
+    # Loop over each PCIAT, predicting it
     for val in pciat_vals:
-        other_vals = [x for x in pciat_vals if x != val]
+        other_vals = [x for x in pciat_vals if x != val] # Make list for remaining values for filtering
         # print(other_vals)
-        train3 = train2.drop(other_vals, axis=1)
-        test3 = test2.drop(other_vals, axis=1)
-        X_train = train3.drop(val, axis=1)
+        train3 = train2.drop(other_vals, axis=1) # Drop other PCIATs so that it can't be predicted from them
+        test3 = test2.drop(other_vals, axis=1) 
+        X_train = train3.drop(val, axis=1) # do the X y split
         X_test = test3.drop(val, axis=1)
         y_train = train3[val]
         y_test = test3[val]
-        # X_train, X_test, y_train, y_test = train_test_split(X, y,  test_size=0.3, random_state=42)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        pciat_pred[val] = y_pred
-    pciat_pred["total"]=pciat_pred[pciat_pred.columns].sum(axis=1)
+        model.fit(X_train, y_train) # Train the model 
+        y_pred = model.predict(X_test) # predict the PCIAT values
+        pciat_pred[val] = y_pred # update the PCIAT prediction
+    
+    # Calculate the total column
+    pciat_pred["total"]=pciat_pred[pciat_pred.columns].sum(axis=1) 
+
+    #Map the total column to sii prediction
+
     y_pred = pciat_pred['total'].apply(cat_sii)
+
+    # Define y_true 
+    
     y_test = test1['sii']
 
     time2 = time.time()
